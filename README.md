43184a26d44ec527ceafc74d13cee8de3359620b
Awhile7127 <gitlab@sbcv.co.uk>
2023-03-13 17:20:19 +0000

    First

---
+++ README.md
@@ -0,0 +1,42 @@
+# pylyrics
+
+
+## Description
+
+Simple web-scraping tool that interacts with [Genius Lyrics](https://genius.com/) to fetch song lyrics by name or URL.
+
+Written in Python.
+
+
+## Compiling
+
+Ensure that `python3` and `pip` are installed.
+
+Then, simply install required packages:
+
+```
+pip3 install -r requirements.txt
+```
+
+
+## Usage
+
+```
+python3 src/main.py --help
+python3 src/main.py <url> <name>
+```
+
+- url **OPTIONAL**: The URL of the server where requests are sent
+- name **OPTIONAL**: The name of the song, following Genius Lyrics format. The song name must be prefixed by the **full name** of the artist. See below for examples.
+
+Lyrics to Ariana Grande's "thank u, next", can be fetched in two ways:
+
+```
+python3 src/main.py --url https://genius.com/Ariana-grande-thank-u-next-lyrics
+
+# OR
+
+python3 src/main.py --name "ariana grande thank u next"
+```
+
+Note that the `--url` and `--name` parameters are mutually exclusive, therefore both cannot be included when running the program.
---
+++ src/main.py
@@ -0,0 +1,76 @@
+# Used for HTML parsing
+from bs4 import BeautifulSoup
+
+
+# HTTP requests, command-line argument parsing
+import requests, argparse
+
+
+# Initialise argparse and declare arguments
+def parse_arguments():
+	parser = argparse.ArgumentParser(description='Fetch song lyrics from genius.com')
+
+	# Create mutually exclusive group, so either the name OR the URL can be provided; not both
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument('--url', '-u', help='URL of the song', nargs=1, default='', required=False)
+	group.add_argument('--name', '-n', help='Name of the song prefixed by the name of the artist', nargs=1, default='', required=False)
+	args = parser.parse_args()
+	return args
+
+
+# Lyrics exist within seperate 'span' elements within one div
+def parse_html(html):
+	soup = BeautifulSoup(html, features='html.parser')
+
+	# Find div with lyrics
+	div = soup.find('div', {'class': 'Lyrics__Container-sc-1ynbvzw-6 YYrds'})
+
+	# Used later to replace '<br>' tags with line breaks
+	delimeter = '\n'
+
+	# Iterate through '<br>' tags in the div
+	for line_break in div.find_all('br'):
+
+		# Replace '<br>' tags with line breaks
+		line_break.replace_with(delimeter)
+
+	# Split the lyrics and store them in an array
+	text = div.get_text().split(delimeter)
+	return text
+
+
+def make_request(args):
+
+	# Declared for later use
+	r = ''
+
+	# If URL provided, make request and parse
+	# If name provided, make request and parse
+	if args.url != '':
+		r = requests.get(args.url[0])
+		if r.status_code != 200:
+			print("Error:", r.status_code)
+			exit()
+		r = r.text
+		lyrics = parse_html(r)
+	elif args.name != '':
+		name = args.name[0]
+		name = name.replace(' ', '-')
+		url = 'https://genius.com/' + name + '-lyrics'
+		r = requests.get(url)
+		if r.status_code != 200:
+			print("Error:", r.status_code)
+			exit()
+		r = r.text
+		lyrics = parse_html(r)
+	return lyrics
+
+
+def main():
+	args = parse_arguments()
+	lyrics = make_request(args)
+	for line in lyrics:
+		print(line)
+
+
+main()
